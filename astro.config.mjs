0b089d5718ee9ff22c397502db256daaa20fef0a
Viktor Popp <vpopp@proton.me>
2026-09-12 10:54:59 +0200

    Fix stylesheets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,23 @@
 // @ts-check
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/styles/variables" as *;`,
+        },
+      },
+    },
+  },
   fonts: [
     {
       name: "IBM Plex Serif",
